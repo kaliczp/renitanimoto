9927d86ff6815a1898fb799aeb8aa053a72451b0
Péter Kalicz <kaliczp@gmail.com>
2016-09-30 12:42:35 +0200

    Működő cluster

--- stat_boku.R
+++ stat_boku.R
@@ -1,12 +1,8 @@
 taxa_nov = read.csv2("Taxa_Nov.csv", fileEncoding="latin2")
+row.names(taxa_nov) <- taxa_nov[ ,1]
+taxa_nov <- taxa_nov[,-1]
 taxa_nov <- t(taxa_nov)
 
-row.names(taxa_nov)
-colnames(taxa_nov) <- taxa_nov[1, ]
-taxa_nov <- taxa_nov[-1, ]
-##row.names(taxa_nov) <- taxa_nov[ ,1]
-##taxa_nov <- taxa_nov[,-1]
-##names(taxa_nov)
 library(plyr)
 
 tanimoto <- function(this, other) {
@@ -30,8 +26,8 @@ tanimoto(taxa_nov[,1],taxa_nov[,2])
 tanimoto.dist <- function(x){
   col.num <- ncol(x)
   dist.mat <- matrix(rep(NA,col.num^2),nrow=col.num)
-  colnames(dist.mat) <- names(x)
-  rownames(dist.mat) <- names(x)
+  colnames(dist.mat) <- colnames(x)
+  rownames(dist.mat) <- colnames(x)
   for(sor in 1:col.num){
     for(oszlop in 1:col.num){
       dist.mat[sor, oszlop] <- tanimoto(x[,sor],x[,oszlop])
@@ -51,15 +47,16 @@ hc.ward.nov <- hclust(ossz.tan.dis.nov, "ward.D") # Ward
 hc.single.nov <- hclust(ossz.tan.dis.nov, "single")
 
 ## távolság leírására
-par(mar = c(5.1, 0.1, 4.1, 0.1))
-plot(hc.comp.nov, cex = 0.5, axes=F, ylab="", main="Complete")
-plot(hc.ward.nov, cex = 0.5, axes=F, ylab="", main="Ward")
+par(mar = c(5.1, 4.1, 4.1, 0.1))
+plot(hc.comp.nov, cex = 0.5, ylab="", main="Complete")
+identify(hc.comp.nov) #?identify.hclust
+plot(hc.ward.nov, cex = 0.5, ylab="", main="Ward")
 
 tan.szaz.nov <- round(as.dist(tan.dist.nov*100),1)
 print(tan.szaz.nov)
 
 ## hasonlóság leírására
-proba=hclust(100-ossz.tan.dis.nov)
-proba2=hclust(100-ossz.tan.dis.nov, "ward.D")
+proba=hclust(100-tan.szaz.nov)
+proba2=hclust(100-tan.szaz.nov, "ward.D")
 plot(proba)
 plot(proba2)
